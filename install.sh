80768673eb7a055fa533c3ce7133899a938c4c95
Mateusz Myalski <mateusz.myalski@gmail.com>
2024-01-19 11:08:25 +0100

    Added bashmark installation and mechanism for update

--- install.sh
+++ install.sh
@@ -4,16 +4,16 @@ read -rp "This script will install dotfiles. Do you want to proceed? (yes/no): "
 
 if [ "$answer" != "yes" ]; then
     echo "Installation aborted."
-    exit 1
+    return 1
 fi
 
-dot_dir="$HOME/.dot"
-dot_backup_dir="$HOME/.dot.bak"
-bashrc_file="$HOME/.bashrc"
-bashrc_backup_file="$HOME/.bashrc.bak"
-source_me_file="$dot_dir/.source_me"
-xresources_file="$HOME/.Xresources"
-xresources_backup_file="$HOME/.Xresources.bak"
+DOT_DIR="$HOME/.dot"
+DOT_BACKUP_DIR="$HOME/.dot.bak"
+BASHRC_FILE="$HOME/.bashrc"
+BASHRC_BACKUP_FILE="$HOME/.bashrc.bak"
+SOURCE_ME_FILE="$DOT_DIR/.source_me"
+XRESOURCES_FILE="$HOME/.Xresources"
+XRESOURCES_BACKUP_FILE="$HOME/.Xresources.bak"
 
 backup_existing() {
     local source_path="$1"
@@ -29,48 +29,50 @@ backup_existing() {
 }
 
 # Step 1: Backup existing ~/.dot
-backup_existing "$dot_dir" "$dot_backup_dir"
+backup_existing "$DOT_DIR" "$DOT_BACKUP_DIR"
 
 # Step 2: Backup existing ~/.bashrc and remove old one
-backup_existing "$bashrc_file" "$bashrc_backup_file"
+backup_existing "$BASHRC_FILE" "$BASHRC_BACKUP_FILE"
 
 # Step 3: Create new .bashrc
 echo "Creating new .bashrc"
 {
     echo "#!/bin/bash"
     echo "# This is a generated .bashrc file"
-    echo "source $source_me_file"
-} > "$bashrc_file"
+    echo "source $SOURCE_ME_FILE"
+} > "$BASHRC_FILE"
 
 # Step 4: Copy .dot folder to ~/.dot (delete if it already exists)
-if [ -d "$dot_dir" ]; then
-    echo "Removing existing $dot_dir"
-    rm -rf "$dot_dir"
+if [ -d "$DOT_DIR" ]; then
+    echo "Removing existing $DOT_DIR"
+    rm -rf "$DOT_DIR"
 fi
 
-echo "Copying .dot to $dot_dir"
-cp -r .dot "$dot_dir"
+echo "Copying $(dirname \$0)/.dot to $DOT_DIR"
+cp -r "$(dirname \$0)"/.dot "$DOT_DIR"
 
 echo "Installation of dotfiles finished successfully."
 
 # Step 6: Backup existing ~/.Xresources and remove old one
-backup_existing "$xresources_file" "$xresources_backup_file"
+backup_existing "$XRESOURCES_FILE" "$XRESOURCES_BACKUP_FILE"
 
 # Step 7: Copy .Xresources file to ~/.Xresources
-cp ".Xresources" "$xresources_file"
+cp ".Xresources" "$XRESOURCES_FILE"
 
-read -rp "Do you want to install related applications? (yes/no): " answer
 
-if [ "$answer" != "yes" ]; then
-    echo "Installation aborted."
-    exit 1
-fi
+# Step 8: Install utility programs via apt-get
+install_apt_programs() {
+    read -rp "Do you want to install related applications via apt? (yes/no): " answer
 
-install_programs() {
-    local programs=("$@")
+    if [ "$answer" != "yes" ]; then
+        echo "Aborted."
+        return 1
+    fi
+
+    local programs_to_install=("fzf" "bat" "minicom" "ranger" "urxvt-unicode")
     
     for program in "${programs[@]}"; do
-        read -p "Do you want to install $program? (yes/no): " answer
+        read -rp "Do you want to install $program? (yes/no): " answer
         if [ "$answer" = "yes" ]; then
             sudo apt install -y "$program"
         else
@@ -79,7 +81,21 @@ install_programs() {
     done
 }
 
-# Step 8: Install utility programs via apt-get
-programs_to_install=("fzf" "bat" "minicom" "ranger")
+install_apt_programs
 
-install_programs "${programs_to_install[@]}"
+# Step 9: Install third-party tools
+install_bashmarks() {
+    read -rp "Do you want to install bashmarks? (yes/no): " answer
+
+    if [ "$answer" != "yes" ]; then
+        echo "Aborted."
+        return 1
+    fi
+
+    local download_dir=""
+    download_dir=$(mktemp -d)
+
+    git clone https://github.com/huyng/bashmarks.git "$download_dir"
+
+    make -C "$download_dir"/install.sh install
+}
